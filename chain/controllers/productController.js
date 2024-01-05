283020e6d1ff9f750042a3a6809a7eb6afc5d235
DeazStar <deadfromtheborn@gmail.com>
2024-01-05 22:29:18 +0300

    make the server release funds

--- chain/controllers/productController.js
+++ chain/controllers/productController.js
@@ -1,4 +1,6 @@
+const axios = require('axios');
 const Product = require('../models/productModel');
+const releaseFunds = require('../scripts/relaseFunds');
 
 const postProduct = async (req, res, next) => {
   const { wallet } = req.user;
@@ -29,7 +31,29 @@ const listProduct = async (req, res, next) => {
   });
 };
 
+const releaseEnergy = async (req, res, next) => {
+  const { deviceId, txtId } = req.body;
+
+  const response = await axios.post(`${process.env.NODEMCU_URL}/send`, {
+    id: deviceId,
+  });
+
+  const data = JSON.parse(response.data);
+
+  if (data === 'success') {
+    await releaseFunds(txtId);
+    await Product.findByIdAndUpdate(txtId, {
+      transactionState: 'sold',
+      purchasedDate: Date.now(),
+    });
+    res.status(200).json({ status: 'success', message: 'Fund released' });
+  }
+
+  res.status(200).json({ status: 'failure', message: 'Can not release funds' });
+};
+
 module.exports = {
   postProduct,
   listProduct,
+  releaseEnergy,
 };
