0bfc56ee60db897ebb496d98373c7400e65ea2dc
DeazStar <deadfromtheborn@gmail.com>
2024-01-05 22:29:27 +0300

    update

--- chain/routes/productRoutes.js
+++ chain/routes/productRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
   postProduct,
   listProduct,
+  releaseEnergy,
 } = require('../controllers/productController');
 const { authRoute } = require('../controllers/authController');
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.use(authRoute);
 router.route('/').get(listProduct).post(postProduct);
+router.route('/release').post(releaseEnergy);
 
 module.exports = router;
