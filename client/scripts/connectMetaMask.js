c2c9ad04dad07be7249d6149dc9cfb3c97f5cbc6
DeazStar <deadfromtheborn@gmail.com>
2024-01-06 21:13:23 +0300

    make it return the signers address

--- client/scripts/connectMetaMask.js
+++ client/scripts/connectMetaMask.js
@@ -4,6 +4,9 @@ export default async function connectMetaMask() {
   if (window.ethereum) {
     const provider = new ethers.BrowserProvider(window.ethereum);
     await provider.send("eth_requestAccounts", []);
+    const signer = await provider.getSigner();
+
+    return signer.address;
   } else {
     throw new Error("No wallet detected. Download MetaMask");
   }
