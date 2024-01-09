351292a5d8abe9abd71679b8c8a40a8bbe0948c8
Biyaol Mesay <101469374+BiyaMe@users.noreply.github.com>
2024-01-09 13:25:21 +0300

    Update Routing

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,24 +1,20 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Home'
-import Sell from './Sell'
-import Buy from './Buy'
-import Login from './Login'
-function App() {
-  const [count, setCount] = useState(0)
+import Home from './Home';
+import Sell from './Sell';
+import Buy from './Buy';
+import Login from './Login';
 
+function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route >
-          <Route index element={<Home />} />
-          <Route path="buy" element={<Buy />} />
-          <Route path="sell" element={<Sell />} />
-          <Route path="login" element={<Login />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/buy" element={<Buy />} />
+        <Route path="/sell" element={<Sell />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
