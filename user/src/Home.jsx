272901862e35a258ff8534c83ccd5d57b8e6c6e2
Biyaol Mesay <101469374+BiyaMe@users.noreply.github.com>
2024-01-09 13:46:56 +0300

    Update Home.jsx routing

--- user/src/Home.jsx
+++ user/src/Home.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 function Home(props) {
 return (
     <div className="bg-slate-900 flex flex-col items-center justify-center h-screen w-screen">
     <header className="flex flex-col items-start">
     <h1 className="text-white text-2xl absolute left-8">BBESP</h1>
-    <div className="self-stretch flex items-stretch justify-end gap-5 max-md:max-w-full max-md:flex-wrap">
-        <a href="login" className="text-white text-center text-xl self-center my-auto">
-            Sign up
-        </a>
-        <a href="#" className="text-white text-center text-xl self-center my-auto">
-            Connect To Your Wallet
-        </a>
-    </div>
+    <nav className="flex items-start justify-between gap-5 self-start max-md:max-w-full max-md:flex-wrap">
+        <Link
+        to="/login"
+        className="text-white text-center text-xl bg-zinc-300 bg-opacity-30 self-stretch grow justify-center items-stretch pl-10 pr-14 py-4 rounded-3xl max-md:px-5"
+        >
+        Sign Up
+        </Link>
+        <Link
+        to="#"
+        className="text-white text-center text-xl bg-zinc-300 bg-opacity-30 self-stretch grow justify-center items-stretch pl-10 pr-14 py-4 rounded-3xl max-md:px-5"
+        >
+        Connect to Wallet
+        </Link>
+    </nav>
     </header>
     <h1 className="text-7xl font-medium tracking-widest bg-clip-text self-center mt-10 max-md:max-w-full max-md:text-4xl max-md:mt-10">
     A Fast Blockchain.
