da1f393875d2a86ccd93d77aca784322aab9056a
Biyaol Mesay <101469374+BiyaMe@users.noreply.github.com>
2024-01-09 13:47:30 +0300

    Update Login.jsx routing

--- user/src/Login.jsx
+++ user/src/Login.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login(props) {
+const navigate = useNavigate();
+const handleSignUp = () => {
+navigate("/sell");
+};
+
 return (
 <div className="bg-slate-900 flex flex-col items-center justify-center h-screen w-screen">
     <header className="flex flex-col items-start">
@@ -18,7 +24,10 @@ return (
         placeholder="Device ID"
         className="text-black text-2xl bg-neutral-100 placeholder-gray-500 hover:bg-neutral-200 text-center px-8 py-4 rounded-[100px] mb-4 w-64 focus:outline-none focus:ring focus:border-cyan-800"
     />
-    <button className="text-white text-center text-xl whitespace-nowrap justify-center items-center bg-cyan-800 w-[212px] mt-8 px-16 py-6 rounded-[100px]">
+    <button
+        className="text-white text-center text-xl whitespace-nowrap justify-center items-center bg-cyan-800 w-[212px] mt-8 px-16 py-6 rounded-[100px]"
+        onClick={handleSignUp}
+    >
         SIGN UP
     </button>
     </div>
