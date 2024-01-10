9409a9b8cde84c6dc04134d457e1f0b9bffe7972
DeazStar <deadfromtheborn@gmail.com>
2024-01-10 23:48:33 +0300

    add morgan and server react up through express

--- chain/app.js
+++ chain/app.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 require('dotenv').config();
 const userRouter = require('./routes/userRoutes');
 const productRouter = require('./routes/productRoutes');
@@ -12,10 +14,15 @@ const corsOptions = {
   credentials: true,
 };
 
+app.use(morgan('dev'));
 app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/product', productRouter);
 
