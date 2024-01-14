c7312eceaed1a25ed9785f21f08edaa005cc90a4
Biyaol Mesay <101469374+BiyaMe@users.noreply.github.com>
2024-01-14 15:14:43 +0300

    user profile added to Sell.jsx

--- client/src/Sell.jsx
+++ client/src/Sell.jsx
@@ -27,6 +27,10 @@ function Sell() {
     <div className="bg-slate-900 flex flex-col items-center justify-center w-screen">
       <header className="flex flex-col items-start">
         <h1 className="text-white text-2xl absolute left-8">BBESP</h1>
+        <div className="flex items-center absolute right-8">
+          <img src="https://e7.pngegg.com/pngimages/782/114/png-clipart-profile-icon-circled-user-icon-icons-logos-emojis-users-thumbnail.png" alt="Profile Icon" className="w-8 h-8 rounded-full" />
+          <span className="text-white text-sm ml-2">{wallet_address}</span>
+        </div>
         <nav className="flex items-start justify-between gap-5 self-start max-md:max-w-full max-md:flex-wrap">
           <Link
             to="/"
